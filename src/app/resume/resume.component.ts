c466743910b06b2885859af598b1a8cb849741b7
abdalhamed9698 <abdalhamed9698@gmail.com>
2024-10-28 01:14:37 +0000

    allow the user to upload photo

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css'],
-  imports: [ReactiveFormsModule] // Import ReactiveFormsModule here
+  imports: [ReactiveFormsModule, CommonModule]
 })
 export class ResumeComponent {
   resumeForm: FormGroup;
+  selectedFile: File | null = null;
 
   constructor() {
     this.resumeForm = new FormGroup({
@@ -21,11 +23,25 @@ export class ResumeComponent {
       education: new FormControl(''),
       workExperience: new FormControl(''),
       workshops: new FormControl(''),
+      photo: new FormControl(''),
     });
+  }
+
+  onFileChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length) {
+      this.selectedFile = input.files[0];
+      this.resumeForm.patchValue({
+        photo: this.selectedFile.name
+      });
+    }
+  }
+
+  createImageUrl(file: File): string {
+    return URL.createObjectURL(file);
   }
 
   onSubmit() {
     console.log(this.resumeForm.value);
-    // Here you can add logic to generate the resume or navigate to a new route
   }
 }
