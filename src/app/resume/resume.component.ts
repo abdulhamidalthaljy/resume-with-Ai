202e4e2079142bc6fa2ad254e34800eda52d487e
abdalhamed9698 <abdalhamed9698@gmail.com>
2024-10-29 15:59:38 +0000

    add the RESUME TEMPLATE AND replace the content to resume directly from  inputs

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-resume',
@@ -25,8 +27,29 @@ export class ResumeComponent {
       workshops: new FormControl(''),
       photo: new FormControl(''),
     });
+    
   }
+  saveAsPDF() {
+    const data = document.getElementById('contentToConvert'); // Replace 'contentToConvert' with the ID of your main content element
+    
+    if (data) {
+      html2canvas(data).then(canvas => {
+        const imgWidth = 208;
+        const pageHeight = 295;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+        const pdf = new jsPDF('p', 'mm', 'a4');
+        const imgData = canvas.toDataURL('image/png');
+  
+        let position = 0;
+        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        pdf.save('webpage.pdf');
+      });
+    }
+  }
+  
 
+  
+      
   onFileChange(event: Event) {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length) {
