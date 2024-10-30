c58277d3654c0ca6145c795355386756186fd58b
abdalhamed9698 <abdalhamed9698@gmail.com>
2024-10-30 00:46:34 +0000

    update abdulhamid

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -3,15 +3,19 @@ import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { jsPDF } from 'jspdf';
 import html2canvas from 'html2canvas';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPhoneAlt,faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import html2pdf from 'html2pdf.js';
 @Component({
   selector: 'app-resume',
   standalone: true,
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css'],
-  imports: [ReactiveFormsModule, CommonModule]
+  imports: [ReactiveFormsModule, CommonModule,FontAwesomeModule]
 })
 export class ResumeComponent {
+  faTeletype = faPhoneAlt;
+  faEnvelope = faEnvelope;
   resumeForm: FormGroup;
   selectedFile: File | null = null;
 
@@ -19,6 +23,8 @@ export class ResumeComponent {
     this.resumeForm = new FormGroup({
       name: new FormControl(''),
       email: new FormControl(''),
+      phone: new FormControl(''),
+
       designation: new FormControl(''),
       college: new FormControl(''),
       about: new FormControl(''),
@@ -29,21 +35,26 @@ export class ResumeComponent {
     });
     
   }
+
   saveAsPDF() {
-    const data = document.getElementById('contentToConvert'); // Replace 'contentToConvert' with the ID of your main content element
+    const element = document.getElementById('contentToConvert');
     
-    if (data) {
-      html2canvas(data).then(canvas => {
-        const imgWidth = 208;
-        const pageHeight = 295;
-        const imgHeight = (canvas.height * imgWidth) / canvas.width;
-        const pdf = new jsPDF('p', 'mm', 'a4');
-        const imgData = canvas.toDataURL('image/png');
+    if (element) {
+      const options = {
+        
+        filename: 'resume.pdf',
+        image: { type: 'jpeg', quality: 1.0 },
+        html2canvas: {
+          scale: 2,
+          background: '#ffffff', // Set a white background
+        },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+      };
   
-        let position = 0;
-        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-        pdf.save('webpage.pdf');
-      });
+      // Use html2pdf to create the PDF
+      html2pdf().from(element).set(options).save();
+    } else {
+      console.error("Element not found!");
     }
   }
   
