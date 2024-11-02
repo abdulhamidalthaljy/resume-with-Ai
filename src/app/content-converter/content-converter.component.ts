466e99b07af1fe05f3885c68056f1f95b6114a06
abdalhamed9698 <abdalhamed9698@gmail.com>
2024-11-02 04:27:39 +0000

    styling make it responsive and add preview section

--- src/app/content-converter/content-converter.component.ts
+++ src/app/content-converter/content-converter.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faPhoneAlt,faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import html2pdf from 'html2pdf.js';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-content-converter',
@@ -13,12 +14,31 @@ export class ContentConverterComponent {
   faTeletype = faPhoneAlt;
   faEnvelope = faEnvelope;
   currentDate: Date = new Date();
-  @Input() resumeForm: any; 
+  @Input() resumeForm: any; // Change 'any' to the specific type if available
   @Input() selectedFile: File | null = null; // To receive selected file
  
   
 
 
+  saveAsPDF() {
+    const element = document.getElementById('contentToConvert');
+
+    if (element) {
+      const options = {
+        filename: 'resume.pdf',
+        image: { type: 'jpeg', quality: 1.0 },
+        html2canvas: {
+          scale: 2,
+          background: '#ffffff',
+        },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+      };
+
+      html2pdf().from(element).set(options).save();
+    } else {
+      console.error("Element not found!");
+    }
+  }
 
   createImageUrl(file: File): string {
     return URL.createObjectURL(file);
